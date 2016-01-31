adcae8489a3451ffaa7eee840656cb889716c25e
Syouhei Tanaka <shou.tanak@gmail.com>
2016-01-31 11:21:03 +0900

    headerとnav共通化

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,71 +1,27 @@
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>記事一覧 | Tanakalab</title>
-    <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
-    <link rel="stylesheet" href="{{ elixir("css/vendor.css") }}">
-    <script src="{{ elixir("js/vendor.js") }}"></script>
-    <script src="{{ elixir("js/app.js") }}"></script>
-    <script>
-      redirectIfNotLogin();
-    </script>
-  </head>
-  <body>
+@extends('layouts.layout')
+<?php $title = 'Recent Posts'; ?>
 
-  <header id="header">
-    <!-- <div id="search"> -->
-    <i class="fa fa-search"></i>
-    <input type="text" id="search-text-box" name="search_query" value="" placeholder="Search">
-    <!-- </div> -->
-  </header>
+@section('content')
+  <h3 id="content-title">
+    <i class="fa fa-file-text-o"></i>
+    Articles
+  </h3>
 
-  <div id="nav">
-    <ul id="nav-ul">
-      <li>
-        <a href="#">
-          <i class="fa fa-pencil fa-2x"></i>
-          <p>NEW</p>
-        </a>
-      </li>
-      <li class="current">
-        <a href="#">
-          <i class="fa fa-home fa-2x"></i>
-          <p>HOME</p>
-        </a>
-      </li>
-      <li>
-        <a href="#">
-          <i class="fa fa-tag fa-2x"></i>
-          <p>TAGS</p>
-        </a>
-      </li>
+  <div id="article-filter">
+    <ul>
+      <li><a href="#">All <span>10</span></a></li>
+      <li><a href="#"><i class="fa fa-thumb-tack"></i> Clips <span>5</span></a></li>
+      <li><a href="#"><i class="fa fa-user"></i> You <span>3</span></a></li>
     </ul>
   </div>
 
-  <div id="content">
-    <h3 id="content-title">
-      <i class="fa fa-file-text-o"></i>
-      Articles
-    </h3>
-
-    <div id="article-filter">
-      <ul>
-        <li><a href="#">All <span>10</span></a></li>
-        <li><a href="#"><i class="fa fa-thumb-tack"></i> Clips <span>5</span></a></li>
-        <li><a href="#"><i class="fa fa-user"></i> You <span>3</span></a></li>
-      </ul>
-    </div>
-
-    <div id="article-list">
-      <ul>
-      </ul>
-    </div>
-
+  <div id="article-list">
+    <ul>
+    </ul>
   </div>
+@stop
 
-
-
+@section('additionalJs')
   <script>
     $(function() {
       Article.all().then(function(data) {
@@ -75,5 +31,4 @@
       })
     })
   </script>
-</body>
-</html>
+@stop
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>{{ $title }} | Tanakalab</title>
+    <link rel="stylesheet" href="{{ elixir("css/app.css") }}">
+    <link rel="stylesheet" href="{{ elixir("css/vendor.css") }}">
+    <script src="{{ elixir("js/vendor.js") }}"></script>
+    <script src="{{ elixir("js/app.js") }}"></script>
+    <script>
+        redirectIfNotLogin();
+    </script>
+</head>
+<body>
+
+<header id="header">
+    <!-- <div id="search"> -->
+    <i class="fa fa-search"></i>
+    <input type="text" id="search-text-box" name="search_query" value="" placeholder="Search">
+    <!-- </div> -->
+</header>
+
+<div id="nav">
+    <ul id="nav-ul">
+        <li>
+            <a href="#">
+                <i class="fa fa-pencil fa-2x"></i>
+                <p>NEW</p>
+            </a>
+        </li>
+        <li class="current">
+            <a href="#">
+                <i class="fa fa-home fa-2x"></i>
+                <p>HOME</p>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa fa-tag fa-2x"></i>
+                <p>TAGS</p>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<div id="content">
+    @yield('content')
+</div>
+
+@yield('additionalJs')
+</body>
+</html>
